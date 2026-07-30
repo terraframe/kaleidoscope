89150653d700ab3c11cb030e77d7a26439c6211a
rrowlands <rrowlands@terraframe.com>
2026-07-30 12:39:08 -0600

    Fix hardcoded spn prefix

--- geo-ai-explorer-api/src/main/java/net/geoprism/geoai/explorer/core/service/prompt/SharedPrompt.java
+++ geo-ai-explorer-api/src/main/java/net/geoprism/geoai/explorer/core/service/prompt/SharedPrompt.java
@@ -50,7 +50,7 @@ public class SharedPrompt
     // Correct
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX obj: <https://spn.geoprism.net#>
+    PREFIX obj: <%1$s#>
     
     SELECT (SUM(?pop2pmu65) as ?totalPopulation) 
     FROM <%2$s>
